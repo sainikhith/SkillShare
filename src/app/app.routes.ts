9dd4c101d52ddfa43ee1faf9ba8c23f68014fbed
Sainikhith <sainikhith98v@gmail.com>
2025-07-24 13:46:41 +0100

    added specific starter files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard-component/dashboard-component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
+    {path: 'dashboard',
+        component: DashboardComponent,
+        canActivate: [] // Add any guards if necessary
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('skillshare');
+  //protected readonly title = signal('skillshare');
 }
