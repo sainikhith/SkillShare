dea47d9860168d4b1d4d420d06c1d911d5f2c525
ricesmg <george.rice-smith@pacsol.co.uk>
2025-07-25 11:29:39 +0100

    Add login, isLoggedIn and isAdmin functionality to authService

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class AuthService {
+  login(role: 'admin' | 'user') {
+    localStorage.setItem('role', role); // Save user role
+  }
 
-  constructor() { }
+  isLoggedIn(): boolean {
+    return !!localStorage.getItem('role'); // Check login
+  }
+
+  isAdmin(): boolean {
+    return localStorage.getItem('role') === 'admin'; // Check admin
+  }
 }
