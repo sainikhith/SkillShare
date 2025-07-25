4df27a721720f05c05dd5bfd3c2df0d422e25892
mrw46 <mrw4632@gmail.com>
2025-07-25 11:41:50 +0100

    MW - home component created with placeholder content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard-component/dashboard-component';
+import { Home } from './pages/home/home';
 
 export const routes: Routes = [
-    {path: '', redirectTo: '/dashboard', pathMatch: 'full'},
-    {path: 'dashboard',
-        component: DashboardComponent,
-        canActivate: [] // Add any guards if necessary
-    }
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'home', component: Home, canActivate: [] },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [], // Add any guards if necessary
+  },
 ];
